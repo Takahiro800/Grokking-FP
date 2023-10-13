6b90d8aabd11d9d9924d040138fb58ba29074ef0
taemath <hirotaka081630@gmail.com>
2023-10-14 06:19:05 +0900

    feat(6.13): optionを返す関数の実装

--- ch06/src/main/scala/tvShows.scala
+++ ch06/src/main/scala/tvShows.scala
@@ -5,34 +5,24 @@ case class TvShow(title: String, start: Int, end: Int) {
     shows.sortBy(tvShow => tvShow.end - tvShow.start).reverse
   }
 
-// Listの解析
-  def parseShows(rawShows: List[String]): List[TvShow] = {
-    rawShows.map(parseShow)
+// 6.13 Optionを返す関数を実装する
+  def parseShow(rawShow: String): Option[TvShow] = {
+    for {
+      name <- extractName(rawShow)
+      yearStart <- extractYearStart(rawShow)
+      yearEnd <- extractYearEnd(rawShow)
+    } yield TvShow(name, yearStart, yearEnd)
   }
 
-// 個別の解析
-  def parseShow(rawShow: String): TvShow = {
-    val bracketOpen = rawShow.indexOf('(')
-    val bracketClose = rawShow.indexOf(')')
-    val dash = rawShow.indexOf('-')
-
-    val name = rawShow.substring(0, bracketOpen).trim()
-    val yearStart = Integer.parseInt(rawShow.substring(bracketOpen + 1, dash))
-    val yearEnd = Integer.parseInt(rawShow.substring(dash + 1, bracketClose))
-
-    TvShow(name, yearStart, yearEnd)
+  def extractName(rawShow: String): Option[String] = {
+    ???
   }
 
-  val rawShows: List[String] = List(
-    "Breaking Bad (2008-2013)",
-    "The Wire (2002-2008)",
-    "Mad Men (2007-2015)"
-  )
+  def extractYearStart(rawShow: String): Option[Int] = {
+    ???
+  }
 
-  parseShows(rawShows)
-
-  // 6.10
-  val invalidRawShow = "Breaking Bad, 2008-2013"
-  parseShow(invalidRawShow)
-
+  def extractYearEnd(rawShow: String): Option[Int] = {
+    ???
+  }
 }
